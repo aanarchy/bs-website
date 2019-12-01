852e1b11dab6974bfccc1443cf38c98b57328a36
aanarchy <unconfigured@null.spigotmc.org>
2019-12-02 04:20:59 +0800

    Fixed nav bar

--- nav.js
+++ nav.js
@@ -2,8 +2,13 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
+//  const content = document.querySelector(".content");
+//  const title = document.querySelector(".title");
 
   burger.addEventListener("click", () => {
+//    content.style.display = "none";
+//    title.style.display = "none";
+
     nav.classList.toggle("sidebar-active");
 
     // Animate links
